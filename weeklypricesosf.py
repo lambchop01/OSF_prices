899e3384fc77a16ee54f9fd076f97e794bbd9657
Kevin <62576245+lambchop01@users.noreply.github.com>
2021-01-26 16:27:28 -0500

    Update weeklypricesosf.py
    funtional

--- weeklypricesosf.py
+++ weeklypricesosf.py
@@ -1,7 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
-from pathlib import Path
+import tabula
+import pandas as pd
+import os
+import logging
+import csv
+import csvsingle
 
+logging.basicConfig(filename='prices.log', filemode='a',format='%(asctime)s - %(message)s',level=logging.INFO)
+# define a Handler which writes INFO messages or higher to the sys.stderr
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+# add the handler to the root logger
+logging.getLogger('').addHandler(console)
 
 headers = {
     'Access-Control-Allow-Origin': '*',
@@ -24,15 +35,50 @@ for file in soup.find_all('p'):
 n=4
 del names[-n:]
 del href[-n:]
+logging.info('Got list of files to download')
 
+outcsv = 'output.csv'
 i=0
-for x in href:
-    filename = Path('./prices/'+str(i)+' '+names[i]+'.pdf')
-    url = 'https://www.ontariosheep.org'+x
-    response = requests.get(url)
-    filename.write_bytes(response.content)
-    i=i+1
-    print(str(i)+' downloaded : '+str(filename))
+all = []
+olex = []
+osi = []
 
+def csvout(outcsv):
+    tabula.convert_into(url, outcsv, output_format="csv", pages="all", guess=False, lattice=True)
+
+def mastercsv(all,olex,osi,n):
+    for week in all:
+        f = open('allprices.csv','a+')
+        f.write(week+'\r\n')
+        f.close
+    for week in olex:
+        f = open('olexprices.csv','a+')
+        f.write(week+'\r\n')
+        f.close
+    for week in osi:
+        f = open('osiprices.csv','a+')
+        f.write(week+'\r\n')
+        f.close
+    logging.info(str(n)+' weeks saved')
+    
+for x in href:
+    url = 'https://www.ontariosheep.org'+x
+
+    csvout(outcsv)
+    
+    name = names[i]
+    i=i+1
+    logging.info(str(i)+' converted : '+str(name))
+    
+    csvsingle.csvstrings(all,olex,osi)
+    logging.debug(all)
+    logging.info('collected: '+name)
+
+logging.info(str(i)+' files downloaded and converted')
+
+n=1
+mastercsv(all,olex,osi,n)
+logging.info('All Done!')
+   
 print('All Done! '+str(i)+' files downloaded')
     
